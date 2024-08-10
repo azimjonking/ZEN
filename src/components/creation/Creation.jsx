9b78caf040f9c3df9cc7b10a6345ac634dbce6ce
zokirjanov2009 <zokirjanovakbarjon846@gmail.com>
2024-08-10 11:47:22 +0500

    home,creation tayyor

--- src/components/creation/Creation.jsx
+++ src/components/creation/Creation.jsx
@@ -3,7 +3,20 @@ import './Creation.css'
 const Creation = () => {
   return (
     <section className='creation'>
-      <h1>Creation</h1>
+      <div className="big">
+        <div className="box-img">
+          <img src="src/assets/img2.png" alt="" />
+        </div>
+        <div className="box-text">
+          <h2>
+          Quick and easy <br /> account creation
+          </h2>
+          <span>Now, you might be thinking that it’s just like any other <br /> account creation… Long, tiresome and full of endless <br /> blanks to fill in. Well, it’s not.</span>
+          <h3>We have designed the whole process to make it the <br /> simplest and the shortest one ever conceived for a <br /> personal account. Download the app and see for yourself!</h3>
+          <img src="src/assets/icon.svg" alt="" />
+          <img src="src/assets/Group (1).svg" alt="" />
+        </div>
+      </div>
     </section>
   )
 }
