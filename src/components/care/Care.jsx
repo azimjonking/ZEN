89ad04db132cc7f107a9b7953f1b9c28a2014151
MOHIRJONKING <abdusattarovmohirzon@mail.com>
2024-08-10 11:49:56 +0500

    care, warranty pages are done

--- src/components/care/Care.jsx
+++ src/components/care/Care.jsx
@@ -1,11 +1,35 @@
-import './Care.css'
+import "./Care.css";
 
 const Care = () => {
   return (
-    <section className='care'>
-      <h1>Care</h1>
-    </section>
-  )
-}
+    <section className="care">
+      <div className="ZEN">
+        <h1>ZEN Care</h1>
 
-export default Care
+        <p>
+          Shopping online is usually a delightful experience. Until <br />
+          it's not. <br />
+
+          Until something you buy, proves to be faulty <br />
+          missing or a totally wrong item, you’re in for <br />
+          long product return procedures. <br />
+
+          But, here comes ZEN Care: a free, built-in shopping <br />
+          protection. You can rest assured we’ll handle all of the <br />
+          transaction-related problems faster and with higher <br />
+          success rates. Where lone consumers can be ignored or <br />
+          dumped by dishonest merchants, ZEN doesn't get <br />
+          discouraged. ZEN persists.
+        </p>
+
+      
+
+      </div>
+      <div className="img"> 
+      <img src="./src/assets/care.png" alt="" />       
+      </div>
+    </section>
+  );
+};
+
+export default Care;
--- src/components/warranty/Warranty.jsx
+++ src/components/warranty/Warranty.jsx
@@ -1,11 +1,31 @@
-import './Warranty.css'
+import "./Warranty.css";
 
 const Warranty = () => {
   return (
-    <section className='warranty'>
-      <h1>Warranty</h1>
-    </section>
-  )
-}
+    <section className="warranty">
 
-export default Warranty
+    <div className="img">
+    <img src="./src/assets/warentey.png" alt="" />
+    </div>
+      <div className="text">
+       <h1>1-year warranty boost</h1>
+
+        <p>
+          ZEN cards have a very neat feature that will help you <br />
+          whenever your electronic devices start failing after the <br />
+          standard warranty ends,which is pretty common.
+        </p>
+
+        <p>
+        When you buy any electronics with your ZEN shopping <br />
+        Mastercard®, their warranty gets extended by one <br />
+         year. How does it work? It simply does. Automatically.
+        </p>
+      </div>
+
+ 
+    </section>
+  );
+};
+
+export default Warranty;
